18314f588719eee6a6825f3a7bebfdac88e0365b
phungaudomsongsakdi-spec <phungaudomsongsakdi@gmail.com>
2026-06-10 13:19:11 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzNhmrdymZzVLfy1RMISiArMyD1jBINPVvTPTbDvW9naNcndaiXxOeCq_OY9sBo0T0s/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbybOsX--wK1wFxsQtwpfqQkIgjstvKrK71-L97hdcVLzYFPoPCpToHhWT_o0auRKNL6/exec",
   APP_VERSION: "2.0.0",
   AUTO_REFRESH_INTERVAL: 30000,
   DEBUG: true  // เพิ่ม debug mode
