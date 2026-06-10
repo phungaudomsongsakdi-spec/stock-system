95f8efa3e8c05c3d582ee086cffaed5cdb0a26b6
phungaudomsongsakdi-spec <phungaudomsongsakdi@gmail.com>
2026-06-10 11:04:46 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
-
+// 👇 IMPORTANT: เปลี่ยน URL เป็น Web App URL ของคุณที่ได้จาก Google Apps Script
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyZEI7AaJ4H2ZPnZe-Mkjii3RgwWKcnM9d0HyHMfEkKb-T6PUJTx9n49yJJ3knznyu7Sw/exec",
+  APP_VERSION: "2.0.0",
+  AUTO_REFRESH_INTERVAL: 30000, // 30 วินาที รีเฟรชอัตโนมัติ
+};
