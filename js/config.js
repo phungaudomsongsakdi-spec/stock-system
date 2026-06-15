7b76419c4d0a9ba19e04408338a18247eb43446c
phungaudomsongsakdi-spec <phungaudomsongsakdi@gmail.com>
2026-06-15 10:15:01 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbybOsX--wK1wFxsQtwpfqQkIgjstvKrK71-L97hdcVLzYFPoPCpToHhWT_o0auRKNL6/exec",
-  APP_VERSION: "2.0.0",
-  AUTO_REFRESH_INTERVAL: 30000,
-  DEBUG: true  // เพิ่ม debug mode
-};
